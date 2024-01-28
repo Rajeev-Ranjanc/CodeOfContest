e75ba87ec9e15ca705101ee2f85e961b8762f5cd
unknown <itrajeevranjan@gmail.com>
2024-01-28 22:53:29 +0530

    codeforces

--- ArrangingCats.java
+++ ArrangingCats.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
-
+//done on codeforces
 public class ArrangingCats {
     public static void main(String[] args) {
-        Scanner sc = new Scanner ( System.in );
+        Scanner sc = new Scanner(System.in);
         //testcase loop
-        int size = sc.nextInt ();
-        sc.nextLine ();
-        String s = sc.nextLine ();
-        String f = sc.nextLine ();
-        
 
-        //end testcase loop
+        int t = sc.nextInt();
+        for (int intt = 0; intt < t; intt++) {
+
+
+            int size = sc.nextInt();
+
+//        sc.nextLine ();
+            String s = sc.next();
+            String f = sc.next();
+            int s_0 = 0;
+            int s_1 = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '1' && f.charAt(i) == '0') {
+                    s_1++;
+                } else if (s.charAt(i) == '0' && f.charAt(i) == '1') {
+                    s_0++;
+                }
+            }
+            int ans = Math.max(s_0, s_1);
+            System.out.println(ans);
+            //end testcase loop
+        }
     }
-
 }
