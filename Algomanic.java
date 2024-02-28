999b307159c771eff4c9f7065b6e45c6319c15a9
unknown <itrajeevranjan@gmail.com>
2024-02-28 20:51:57 +0530

    codechef

---
+++ Algomanic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//https://www.codechef.com/START123D/problems/ALGOFINALS
+public class Algomanic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x==17){
+            System.out.println("YAY");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
